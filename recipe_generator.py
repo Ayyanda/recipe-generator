a98fab8377398d504d7e47efe72b01620233504e
aykhumajhb024 <aykhumajhb024@student.wethinkcode.co.za>
2025-03-06 01:47:25 +0200

    promted for continuous user input of available ingredients and stored them in a list

---
+++ recipe_generator.py
@@ -0,0 +1,29 @@
+import requests
+
+API_key = "47c8a1a57ba24939bb25b2357340f3e3"
+
+response = requests.get("https://api.spoonacular.com/recipes/complexsearch")
+
+def generate_ingredients():
+    ingredients = []
+
+    while True:
+        try:
+            ingredient = input("Enter the ingredients you have ").strip().lower()
+            if ingredient.lower() == "finished":
+                break
+
+            ingredients.append(ingredient)
+
+            
+        except EOFError:
+            break
+
+    return ingredients
+            
+        
+print(generate_ingredients())
+
+        
+
+    
