f17db0effeba8e2cc912ba144deac0202b5f9d49
aykhumajhb024 <aykhumajhb024@student.wethinkcode.co.za>
2025-06-22 18:37:21 +0200

    commented out ingredients.py file as i already am generating the ingredients in my main function and handled conditional for when user types finished when they are done entering their ingredients

--- RecipeGenerator/ingredients.py
+++ RecipeGenerator/ingredients.py
@@ -1,24 +1,24 @@
-# This file handles all ingredient-related functionality
+# # This file handles all ingredient-related functionality
 
-def generate_ingredients():
-    """This function gets ingredients from user input"""
-    ingredients = []
+# def generate_ingredients():
+#     """This function gets ingredients from user input"""
+#     ingredients = []
 
-    print("Enter the ingredients you have (type 'finished' when done):")
+#     print("Enter the ingredients you have (type 'finished' when done):")
     
-    while True:
-        try:
-            ingredient = input("Enter ingredient: ").strip().lower()
+#     while True:
+#         try:
+#             ingredient = input("Enter ingredient: ").strip().lower()
 
-            if ingredient == "finished":
-                break
+#             if ingredient == "finished":
+#                 break
 
-            if ingredient:
-                ingredients.extend([i.strip() for i in ingredient.split(",") if i.strip()])
-            else:
-                print("Please enter a valid ingredient.")
+#             if ingredient:
+#                 ingredients.extend([i.strip() for i in ingredient.split(",") if i.strip()])
+#             else:
+#                 print("Please enter a valid ingredient.")
 
-        except EOFError:
-            break
+#         except EOFError:
+#             break
 
-    return ingredients
+#     return ingredients
